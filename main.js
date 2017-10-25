244fc0bb14935dc7ee04db4f7f1410c6efa63de2
Viaka <rharper9@gmail.com>
2017-10-25 16:29:02 -0500

    Sketch field operable
    grid squares go opaque

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
-	alert($("#text-box").val());
-
+	setSize($("#text-box").val());
 
 	document.getElementById("setSize").onclick = function() {
-		setSize($(this).closest('.page').find('#text-box').val());
+		var gridSize = $(this).closest('.page').find('#text-box').val();
+		alert('Grid size set to a ' + gridSize + ' x ' + gridSize + ' square.');
+		setSize(gridSize);
 	};
 
 	function setSize(gridSize) {
-	  alert(gridSize);
-	  for(let i = 0; i < gridSize**2; i++){
-		$('#container').append('<div class="gridSquare"></div>');
-		
-	  }
-	  $('.gridSquare').height(500/gridSize-2);
-	  $('.gridSquare').width(500/gridSize-2);
+		$('#container').empty();
+		for(let i = 0; i < gridSize**2; i++){
+			$('#container').append('<div class="gridSquare"></div>');
+		}
+	  	$('.gridSquare').height(500/gridSize-2);
+		$('.gridSquare').width(500/gridSize-2);
 	}
+	$('#container').on('mouseenter', '.gridSquare', function() {
+		//$(this).addClass('highlight');
+		$(this).animate({'opacity': '0'});
+	});
+	
 });
